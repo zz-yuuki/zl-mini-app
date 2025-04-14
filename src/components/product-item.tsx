c0d252fbf779236d78dd96e90aeab87b507bdb50
Yuuki <kakashi16023@gmail.com>
2025-04-14 16:29:42 +0700

    fix form datetime

--- src/components/product-item.tsx
+++ src/components/product-item.tsx
@@ -7,7 +7,7 @@ import { userInfoKeyState, userInfoState } from "@/state";
 import { useAtom, useAtomValue, useSetAtom } from "jotai";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
-import { Button, Input } from "zmp-ui";
+import { Button, DatePicker, Input } from "zmp-ui";
 import axios from "axios";
 
 export interface ProductItemProps {
@@ -24,23 +24,58 @@ export default function ProductItem(props: ProductItemProps) {
   const userInfo = useAtomValue(userInfoState);
   const setUserInfoKey = useSetAtom(userInfoKeyState);
   const refreshUserInfo = () => setUserInfoKey((key) => key + 1);
-  const [name, setName] = useState('');
-  const [phone, setPhone] = useState('');
-  const [email, setEmail] = useState('');
-  const [address, setAddress] = useState('');
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [date1, setDate1] = useState<Date | undefined>(new Date());
+  const [date2, setDate2] = useState("");
+
+  const [email, setEmail] = useState("");
+  const [address, setAddress] = useState("");
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    const data1 ={
-      Name: name,
-      Phone: phone,
-      Email: email,
-      Address: address,
-      Date: new Date().toString(),
-    }
-    axios.post('https://sheetdb.io/api/v1/qocyk8usx81gw', data1).then((respose) =>{console.log(respose)})
-   
+    // const data1 ={
+    //   Name: name,
+    //   Date: date1,
+    //   Date2: date2,
+    //   Phone: phone,
+    //   Email: email,
+    //   Address: address,
+    // }
+    axios
+      .post("https://sheetdb.io/api/v1/qocyk8usx81gw", formData)
+      .then((respose) => {
+        console.log(respose);
+      });
+
     toast.success("Đã đăng ký thành công!");
-  }
+    navigate(-1);
+  };
+  const getTodayDate = () => {
+    const d = new Date();
+    const year = d.getFullYear();
+    const month = String(d.getMonth() + 1).padStart(2, '0');
+    const day = String(d.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
+  };
+  const [formData, setFormData] = useState({
+    name: "",
+    date: getTodayDate(), // Will be stored as a string
+    phone: "",
+    email: "",
+    address: "",
+  });
+  const handleChange = (key, value) => {
+    console.log(`Changed: ${key} =>`, value);
+    setFormData((prev) => ({ ...prev, [key]: value }));
+  };
+  const formatDateLocal = (date) => {
+    const year = date.getFullYear();
+    const month = `${date.getMonth() + 1}`.padStart(2, "0");
+    const day = `${date.getDate()}`.padStart(2, "0");
+    return `${year}-${month}-${day}`;
+  };
+  
 
   // return (
   //   <form
@@ -60,63 +95,57 @@ export default function ProductItem(props: ProductItemProps) {
   //       toast.success("Đã đăng ký thành công!");
   //       navigate(-1);
   //       //
-      
+
   //     }}
   //   >
-  //     <div className="bg-section p-4 grid gap-4">
-  //       <Input name="name" label="Họ tên" defaultValue={userInfo?.name} />
-  //       <Input
-  //         name="phone"
-  //         label="Số điện thoại"
-  //         required
-  //         defaultValue={userInfo?.phone}
-  //       />
-  //       <Input
-  //         name="email"
-  //         label="Email"
-  //         placeholder="Email"
-  //         defaultValue={userInfo?.email}
-  //       />
-  //       <Input
-  //         name="address"
-  //         label="Địa chỉ"
-  //         placeholder="Nhập dịa chỉ"
-  //         defaultValue={userInfo?.address}
-  //       />
-  //     </div>
-  //     <div className="p-6 pt-4 bg-section">
-  //       <Button htmlType="submit" fullWidth>
-  //         Đăng ký khám
-  //       </Button>
-  //     </div>
   //   </form>
   // );
   return (
     <form
       className="h-full flex flex-col justify-between"
       onSubmit={handleSubmit}
-        
-      
     >
       <div className="bg-section p-4 grid gap-4">
-        <Input name="name" label="Họ tên*" placeholder="Nhập họ tên" required onChange={(e) => setName(e.target.value)} value={name}/>
+        <Input
+          name="name"
+          label="Họ tên*"
+          placeholder="Nhập họ tên"
+          required
+          onChange={(e) => handleChange("name", e.target.value)}
+          value={formData.name}
+        />
+        <DatePicker
+          key={date1?.getDate()}
+          label="Ngày khám*"
+          helperText="Chọn ngày khám"
+          mask
+          maskClosable
+          title="Chọn ngày"
+          value={date1}
+          onChange={(dateObj) => {
+            const localDate = formatDateLocal(new Date(dateObj));
+            handleChange("date", localDate);
+          }}
+        />
         <Input
           name="phone"
           label="Số điện thoại*"
           required
-          onChange={(e) => setPhone(e.target.value)} value={phone}
+          placeholder="Nhập số điện thoại"
+          pattern="[0-9]*"
+          onChange={(e) => handleChange('phone',e.target.value)} value={formData.phone}
         />
         <Input
           name="email"
           label="Email"
           placeholder="Email"
-          onChange={(e) => setEmail(e.target.value)} value={email}
+          onChange={(e) => handleChange('email',e.target.value)} value={formData.email}
         />
         <Input
           name="address"
           label="Địa chỉ"
           placeholder="Nhập dịa chỉ"
-          onChange={(e) => setAddress(e.target.value)} value={address}
+          onChange={(e) => handleChange('address',e.target.value)} value={formData.address}
         />
       </div>
       <div className="p-6 pt-4 bg-section">
